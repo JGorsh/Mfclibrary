7f817207fce77a0fffe2f381ba74bc4bf82188b1
asgor <alexmilesjg@gmail.com>
2022-12-15 17:22:19 +0500

    dto

---
+++ src/main/java/ru/mfc/mfcreference/dto/UnitServiceDto.java
@@ -0,0 +1,59 @@
+package ru.mfc.mfcreference.dto;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UnitServiceDto {
+    private List<UnitServiceDto> children = new ArrayList<>();
+
+    private List<UnitServiceDto> childrenGroups = new ArrayList<>();
+
+    private String description;
+
+    private Long id;
+
+    private String name;
+
+    public List<UnitServiceDto> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<UnitServiceDto> children) {
+        this.children = children;
+    }
+
+    public List<UnitServiceDto> getChildrenGroups() {
+        return childrenGroups;
+    }
+
+    public void setChildrenGroups(List<UnitServiceDto> childrenGroups) {
+        this.childrenGroups = childrenGroups;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
