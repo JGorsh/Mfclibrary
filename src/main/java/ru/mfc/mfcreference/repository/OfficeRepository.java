9b6c025b2670f9ce94280e0b7c1ff612fb16d2a9
asgor <alexmilesjg@gmail.com>
2022-12-21 10:37:41 +0500

    /units check isactive

--- src/main/java/ru/mfc/mfcreference/repository/OfficeRepository.java
+++ src/main/java/ru/mfc/mfcreference/repository/OfficeRepository.java
@@ -3,7 +3,6 @@ package ru.mfc.mfcreference.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.mfc.mfcreference.dto.OfficeNameDto;
 import ru.mfc.mfcreference.entities.Office;
 import java.util.List;
 
@@ -11,7 +10,7 @@ import java.util.List;
 @Repository
 public interface OfficeRepository extends JpaRepository<Office, Long> {
 
-    @Query(value="SELECT * FROM office ", nativeQuery = true)
+    @Query(value="SELECT * FROM office WHERE office_is_active = false", nativeQuery = true)
     List<Office> findAllOffice();
 
     Office getOfficeByOfficeId(String officeId);
