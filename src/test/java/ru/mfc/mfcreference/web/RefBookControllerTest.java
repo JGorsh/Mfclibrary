f7f413fb6308dc18e6a82ac36fa9a0b779998387
asgor <alexmilesjg@gmail.com>
2022-12-23 11:58:58 +0500

    add test controller

--- src/test/java/ru/mfc/mfcreference/web/RefBookControllerTest.java
+++ src/test/java/ru/mfc/mfcreference/web/RefBookControllerTest.java
@@ -1,15 +1,16 @@
 package ru.mfc.mfcreference.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(RefBookController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RefBookControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void resultsList() {
